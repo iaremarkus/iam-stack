2c4ed30b009abec59cfc2585a6fe75aaea64350f
Mark Bloomfield <mark@yellow-llama.com>
2025-02-14 22:30:33 +0200

    feat: prisma db init

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const mark = await prisma.user.upsert({
+    where: { email: "mark@yellow-llama.com" },
+    update: {},
+    create: {
+      email: "", // your email
+      firstName: "", // your first name
+      lastName: "", // your last name
+      role: "SUPER", // your role
+    },
+  });
+
+  console.log({ mark });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
